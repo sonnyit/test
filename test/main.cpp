66d19ff220e87a70f323b7daae50a692f332944e
minhnh13 <minhnh13@fsoft.com.vn>
2015-06-03 13:53:54 +0100

    reconstruct master

--- test/main.cpp
+++ test/main.cpp
@@ -5,22 +5,11 @@
  *      Author: MinhNH13
  */
 #include <iostream>
-#include <cstring>
-#include <stdint.h>
-
-int add(int a, int b) {
-	return a + b;
-}
+#include <memory>
 
 int main(int argc, char **argv)
 {
 	using namespace std;
-	char str[] = "this is a test sentence.";
-	if (strncmp(&str[5], "is", 2) == 0)
-		cout << "same" << endl;
-	else
-		cout << "not same" << endl;
-
 
 	return 0;
 }
