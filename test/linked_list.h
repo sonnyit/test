5996b17fb326df96bc977353fceeed1033a41bfe
minhnh13 <minhnh13@fsoft.com.vn>
2015-06-05 11:09:30 +0100

    runable single linked list

--- test/linked_list.h
+++ test/linked_list.h
@@ -1,28 +1,75 @@
 #ifndef _LINKED_LIST_H_
 #define _LINKED_LISH_H_
+#include <iostream>
 
-template <typename E>
-class SNode
-{
-  private:
-    E element;
-    SNode<E> *next_;
-    friend class SLinkedList<E>;
+template<typename E>
+class SNode {
+public:
+	SNode(const E &data) : data_(data), next_(nullptr) {}
+	virtual ~SNode() {}
+
+	void set_data(const E &data) {
+		data_ = data;
+	}
+
+	const E& get_data() const {
+		return data_;
+	}
+
+	SNode<E> *get_next() const {
+		return next_;
+	}
+
+	void set_next(SNode<E> *node)
+	{
+		next_ = node;
+	}
+private:
+	E data_;
+	SNode<E> *next_;
 };
 
-template <typename E>
-class SLinkedList
-{
-  public:
-    SLinkedList();
-    virtual ~SLinkedList();
-    bool empty() const;
-    const E& front() const;
-    void add_front(const E& e);
-    void remove_front();
+template<typename E>
+class SLinkedList {
+public:
+	SLinkedList() :
+			head_(nullptr) {
+	}
 
-  private:
-    SNode<E> *head;
+	virtual ~SLinkedList() {
+		while (!empty())
+		{
+			std::cout << "front not empty" << std::endl;
+			remove_front();
+		}
+	}
+
+	bool empty() const
+	{
+		return head_ == nullptr;
+	}
+
+	E front() const
+	{
+		return head_->get_data();
+	}
+
+	void add_front(const E& e)
+	{
+		SNode<E> *node = new SNode<E>(e);
+		node->set_next(head_);
+		head_ = node;
+	}
+
+	void remove_front()
+	{
+		SNode<E> *new_head = head_->get_next();
+		delete head_;
+		head_ = new_head;
+	}
+
+private:
+	SNode<E> *head_;
 };
 
 #endif /* _LINKED_LISH_H_ */
--- test/main.cpp
+++ test/main.cpp
@@ -5,12 +5,17 @@
  *      Author: MinhNH13
  */
 #include <iostream>
-#include <memory>
+#include "linked_list.h"
 
 int main(int argc, char **argv)
 {
 	using namespace std;
-
+	SLinkedList<int> linked;
+	linked.add_front(5);
+	linked.add_front(8);
+	cout << linked.front() << endl;
+	linked.remove_front();
+	cout << linked.front() << endl;
 	return 0;
 }
 
