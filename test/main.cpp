be7cf6659519f2ee64c9d38a2126ee631915579e
minh nguyen <nhmjnh@gmail.com>
2015-07-05 23:26:50 +0100

    runable

--- test/main.cpp
+++ test/main.cpp
@@ -11,8 +11,13 @@ int main(int argc, char **argv)
 {
 	using namespace std;
   
-  struct ListNode *a = new ListNode();
+  struct ListNode *a = new ListNode({4, NULL});
+  InsertInLinkedList(&a, 5, 2);
+  InsertInLinkedList(&a, 10, 3);
+  DeleteNodeFromLinkedList(&a, 3);
+  DeleteLinkedList(&a);
   cout << "list length: " << ListLength(a) << endl;
+  PrintLinkedList(a);
 
 	return 0;
 }
--- test/single_linked_list.h
+++ test/single_linked_list.h
@@ -1,3 +1,4 @@
+#include <iostream>
 
 struct ListNode
 {
@@ -17,13 +18,76 @@ int ListLength(struct ListNode *head)
   return count;
 }
 
-void InsertInLinkedList(struct ListNode *head, int data, int position)
+void InsertInLinkedList(struct ListNode **head, int data, int position)
 {
   struct ListNode *new_node = new ListNode();
   new_node->data = data;
 
   if (position == 1)
   {
+    new_node->next = *head;
+    *head = new_node;
+  }
+  else 
+  {
+    struct ListNode *p = *head;
+    int k = 1;
+    while(k < position-1 && p != NULL)
+    {
+      k++;
+      p = p->next;
+    }
+    new_node->next = p->next;
+    p->next = new_node;
+  }
+}
 
+void DeleteNodeFromLinkedList(struct ListNode **head, int position)
+{
+  struct ListNode *p, *q;
+  if (position == 1)
+  {
+    p = *head;
+    *head = (*head)->next;
+    delete p;
+  }
+  else
+  {
+    int k = 1;
+    p = *head;
+    while(k < position-1 && p!=NULL)
+    {
+      k++;
+      p = p->next;
+    }
+    if (p==NULL) {
+      std::cout << "null \n";
+      return;
+    }
+    q = p->next;
+    p->next = q->next;
+    delete q;
+  }
+}
+
+void DeleteLinkedList(struct ListNode **head)
+{
+  struct ListNode *p = *head;
+  while (p != NULL)
+  {
+    *head = (*head)->next;
+    delete p;
+    p = *head;
+  }
+}
+
+void PrintLinkedList(struct ListNode *head)
+{
+  using namespace std;
+  struct ListNode *p = head;
+  while (p != NULL)
+  {
+    cout << p->data << endl;
+    p = p->next;
   }
 }
